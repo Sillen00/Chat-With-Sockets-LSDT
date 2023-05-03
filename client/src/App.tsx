eacb87e1adb08ee274ed3ebb2a98b8d74a607ddc
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-05-03 10:40:12 +0200

    Init bootstrap, base css, folders client side.
    Co-authored-by: Vanpalo <Knightrider85@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Col, Row, Container } from 'react-bootstrap';
+import Header from './components/navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+function App() {
+  return (
+    <div className='d-flex'>
+      <Container fluid>
+        <Row>
+          <Col md={3} className='bg-light'>
+            <Header />
+            {/* Aside on the left */}
+          </Col>
+          <Col md={6} className='bg-white'>
+            <h1>mitten</h1>
+            {/* Main content in the middle */}
+          </Col>
+          <Col md={3} className='bg-light'>
+            <h1>höger</h1>
+            {/* Aside on the right */}
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default App
+
+
+export default App;
