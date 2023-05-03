318f261a3ae15ab19e233149cef5fa21b427f3b0
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-05-03 12:00:26 +0200

    basic navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,49 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
+import chatterapplogo from '../assets/chatterapplogo.svg';
 function Header() {
   return (
     <>
-      <Navbar bg="light">
-        <Container>
-          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar bg="light">
-        <Container>
-          <Navbar.Brand>Brand text</Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar bg="dark">
+      <Navbar bg="dark">       
         <Container>
           <Navbar.Brand href="#home">
             <img
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
+              alt="logo"
+              src={chatterapplogo}
+              width="100"
               className="d-inline-block align-top"
             />{' '}
-            React Bootstrap
           </Navbar.Brand>
         </Container>
-      </Navbar>
+      </Navbar>      
     </>
   );
 }
