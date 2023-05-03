8e4b03adfadb8adc9898d039d2f73e5fdd3998ea
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-05-03 13:16:57 +0200

    create a user and redirect to chat

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,30 +1,45 @@
-import { useSocket } from "../context/SocketContext";
-import { useState } from "react";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSocket } from '../context/SocketContext';
 
 interface User {
   username: string;
 }
 
 export function LoginForm() {
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState('');
   const { socket } = useSocket();
+  const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (username.trim() === "") {
+    if (username.trim() === '') {
       return;
     }
     const user: User = { username };
-    socket.emit("join", user);
+    socket.emit('join', user);
+    navigate('/chat', { state: { username } });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <button type="submit">Join Chat</button>
+    <form onSubmit={handleSubmit} className='d-flex justify-content-center'>
+      <div className='form-group'>
+        <label htmlFor='username'>Username:</label>
+        <input
+          type='text'
+          className='form-control'
+          id='username'
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+      <button
+        type='submit'
+        className='btn btn-primary mt-4'
+        style={{ margin: '1rem', backgroundColor: '#710193 color: #fff' }}
+      >
+        Join Chat
+      </button>
     </form>
   );
 }
