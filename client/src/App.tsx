8bdfc4ed001590f6f00abd23b172a34a62c0c9ce
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-05-03 12:01:07 +0200

    implement reacr-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,34 @@
-import { Col, Row, Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 import Header from './components/navbar';
 
 function App() {
   return (
-function App() {
-  return (
-    <div className='d-flex'>
-      <Container fluid>
-        <Row>
-          <Col md={3} className='bg-light'>
-            <Header />
-            {/* Aside on the left */}
-          </Col>
-          <Col md={6} className='bg-white'>
-            <h1>mitten</h1>
-            {/* Main content in the middle */}
-          </Col>
-          <Col md={3} className='bg-light'>
-            <h1>höger</h1>
-            {/* Aside on the right */}
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <>
+      <div>
+        <main>
+          <Header />
+          <Outlet />
+        </main>
+      </div>
+    </>
   );
 }
 
-
-
 export default App;
+
+// {/* <Container fluid>
+//       <Row style={{ display: 'flex' }}>
+//         <Col md={3} className=''>
+//           <Header />
+//           {/* Aside on the left */}
+//         </Col>
+//         <Col md={6} className='bg-danger'>
+//           <h1>mitten</h1>
+//           {/* Main content in the middle */}
+//         </Col>
+//         <Col md={3} className='bg-light'>
+//           <h1>höger</h1>
+//           {/* Aside on the right */}
+//         </Col>
+//       </Row>
+//     </Container> */}
