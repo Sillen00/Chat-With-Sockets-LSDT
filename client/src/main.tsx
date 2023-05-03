48b9657f13fe20ad288278b19f5ca2b395d03620
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-05-03 12:00:37 +0200

    added routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import SocketProvider from "./context/SocketContext";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import App from './App';
+import SocketProvider from './context/SocketContext';
+import './index.css';
+import Login from './pages/login';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<App />} />
+      <Route path='/login' element={<Login />} />
+    </>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <SocketProvider>
-      <App />
+      <RouterProvider router={router} />
     </SocketProvider>
   </React.StrictMode>
 );
