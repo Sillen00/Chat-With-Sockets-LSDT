90e528343b42d596fc48865fad10cae767a05425
danielvanpalo <daniel@grevensgastro.se>
2023-05-04 21:26:28 +0200

    login styled

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSocket } from '../context/SocketContext';
+import chatterappLogo from '../assets/chatterapplogo.svg';
 
 interface User {
   username: string;
@@ -22,24 +23,30 @@ export function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className='d-flex justify-content-center'>
-      <div className='form-group'>
-        <label htmlFor='username'>Username:</label>
-        <input
-          type='text'
-          className='form-control'
-          id='username'
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-      </div>
-      <button
-        type='submit'
-        className='btn btn-primary mt-4'
-        style={{ margin: '1rem', backgroundColor: '#710193 color: #fff' }}
+    <div className='login-container'>
+      <form
+        onSubmit={handleSubmit}
+        className='d-flex flex-column justify-content-center align-items-center'
       >
-        Join Chat
-      </button>
-    </form>
+        <div className='form-group'>
+          <img src={chatterappLogo} alt='logo' />
+          <label htmlFor='username'>Username:</label>
+          <input
+            type='text'
+            className='form-control'
+            id='username'
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+        </div>
+        <button
+          type='submit'
+          className='btn btn-primary mt-4'
+          style={{ margin: '1rem', backgroundColor: '#710193 color: #fff' }}
+        >
+          Join Chat
+        </button>
+      </form>
+    </div>
   );
 }
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,14 +1,15 @@
-import { LoginForm } from '../components/LoginForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { LoginForm } from '../components/LoginForm';
+import chatterappLogo from '../assets/chatterapplogo.svg';
 
 
 function Login() {
   return (
-    <>
+    <div className='login-body'>
       <div className='d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>
         <LoginForm />
       </div>
-    </>
+    </div>
   );
 }
 
