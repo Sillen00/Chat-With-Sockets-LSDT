e02a0c0e724cea7c63ef69f93b378294d41ad595
danielvanpalo <daniel@grevensgastro.se>
2023-05-04 20:33:57 +0200

    implemented buttongroup in offcanvas

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,47 +1,62 @@
-import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { Button, ButtonGroup, Nav } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import chatterappLogo from '../assets/chatterapplogo.svg';
-import DirectMessage from './Dm';
-import Rooms from './Rooms';
-import '../index.css'
+import '../index.css';
+import DirectMessage, { dm } from './Dm';
+import Rooms, { allRooms } from './Rooms';
 
 function Header() {
+  const [showRooms, setShowRooms] = useState(true);
+
+  const toggleRooms = () => {
+    setShowRooms(!showRooms);
+  };
+
   return (
     <>
-      <Navbar id='custom-navbar' expand='md' sticky='top' >
-        <Container id='fluid-desk' fluid >
+      <Navbar id='custom-navbar' expand='md' sticky='top'>
+        <Container id='fluid-desk' fluid>
           <div className=' d-flex align-items-center flex-row'>
             <img src={chatterappLogo} alt='logo' width='100' />
             {/* <div className='hide-on-mobile'>
             <DirectMessage />
             <Rooms />
             </div> */}
-          </div>  
+          </div>
           <div>
-          <Navbar.Toggle aria-controls='offcanvasNavbar-expand-lg' />
-          <Navbar.Offcanvas style={{ backgroundColor: "#F0E6DC" }}
-            id='offcanvasNavbar-expand-lg'
-            aria-labelledby='offcanvasNavbarLabel-expand-lg'
-            placement='end'
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id='offcanvasNavbarLabel-expand-lg'>
-                Offcanvas
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav id='offcanvas-content' className='justify-content-end flex-grow-1 pe-3' style={{display: 'flex', gap: '1rem'}}>        
-                <DirectMessage />
-                <Rooms />
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-
+            <Navbar.Toggle aria-controls='offcanvasNavbar-expand-lg' />
+            <Navbar.Offcanvas
+              style={{ backgroundColor: '#F0E6DC' }}
+              id='offcanvasNavbar-expand-lg'
+              aria-labelledby='offcanvasNavbarLabel-expand-lg'
+              placement='end'
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id='offcanvasNavbarLabel-expand-lg'>Offcanvas</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav
+                  id='offcanvas-content'
+                  className='justify-content-end flex-grow-1 pe-3'
+                  style={{ display: 'flex', gap: '1rem' }}
+                >
+                  <ButtonGroup aria-label='Buttongroup for room or DM'>
+                    <Button variant={showRooms ? 'dark' : 'light'} onClick={toggleRooms}>
+                      Rooms
+                    </Button>
+                    <Button variant={!showRooms ? 'dark' : 'light'} onClick={toggleRooms}>
+                      DM
+                    </Button>
+                  </ButtonGroup>
+                  {showRooms
+                    ? allRooms.map(room => <div key={room.id}>{room.name}</div>)
+                    : dm.map(message => <div key={message.id}>{message.name}</div>)}
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
           </div>
         </Container>
       </Navbar>
@@ -50,5 +65,3 @@ function Header() {
 }
 
 export default Header;
-
-
